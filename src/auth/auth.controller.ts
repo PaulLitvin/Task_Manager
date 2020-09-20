dd2ba8ac399ed4c3cc3fe46ccef7747706aa4331
Pavlo_Lytvyn <pashalitvin10@gmail.com>
2020-09-20 23:37:31 +0300

    task/TM-6 added sign in functionality and hashing of password

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, ValidationPipe } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthCredentialsDTO } from './dto/auth-credentials.dto';
 
@@ -7,7 +7,20 @@ export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('/signup')
-  signUp(@Body() authCredentialsDTO: AuthCredentialsDTO): Promise<void> {
+  signUp(
+    /**
+     * ValidationPipe validation described in auth-credentials.dto.ts
+     * checked userName and password
+     */
+    @Body(ValidationPipe) authCredentialsDTO: AuthCredentialsDTO,
+  ): Promise<void> {
     return this.authService.signUp(authCredentialsDTO);
+  }
+
+  @Post('/signin')
+  signIn(
+    @Body(ValidationPipe) authCredentialsDTO: AuthCredentialsDTO,
+  ): Promise<void> {
+    return this.authService.signIn(authCredentialsDTO);
   }
 }
